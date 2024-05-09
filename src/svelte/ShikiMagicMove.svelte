c135e0a07e5d358a7730c23d4cf9baa2674507f2
Paolo Ricciuti <ricciutipaolo@gmail.com>
2024-05-09 17:42:19 +0200

    fix: svelte types generation during build (#14)
    Co-authored-by: mattcroat <mattcroat.dev@gmail.com>
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- src/svelte/ShikiMagicMove.svelte
+++ src/svelte/ShikiMagicMove.svelte
@@ -1,32 +1,32 @@
-<script lang="ts">
-	import { codeToKeyedTokens, createMagicMoveMachine } from '../core'
-	import ShikiMagicMoveRenderer from './ShikiMagicMoveRenderer.svelte'
-	import type { HighlighterCore } from 'shiki/core'
-	import type { MagicMoveRenderOptions, MagicMoveDifferOptions } from '../types'
-	
-	interface ShikiMagicMoveProps {
-		highlighter: HighlighterCore
-		lang: string
-		theme: string
-		code: string
-		options?: MagicMoveRenderOptions & MagicMoveDifferOptions
-		onStart?: () => void
-		onEnd?: () => void
-	}
+<script lang='ts'>
+  import { codeToKeyedTokens, createMagicMoveMachine } from 'shiki-magic-move/core'
+  import type { HighlighterCore } from 'shiki/core'
+  import type { MagicMoveDifferOptions, MagicMoveRenderOptions } from 'shiki-magic-move/types'
+  import ShikiMagicMoveRenderer from './ShikiMagicMoveRenderer.svelte'
 
-	const { highlighter, lang, theme, code, options, onStart, onEnd, ...props }: ShikiMagicMoveProps = $props()
-	const machine = createMagicMoveMachine(
-		(code) => codeToKeyedTokens(highlighter, code, { lang, theme }, options?.lineNumbers),
-		options
-	)
-	const result = $derived(machine.commit(code))
+  interface ShikiMagicMoveProps {
+    highlighter: HighlighterCore
+    lang: string
+    theme: string
+    code: string
+    options?: MagicMoveRenderOptions & MagicMoveDifferOptions
+    onStart?: () => void
+    onEnd?: () => void
+  }
+
+  const { highlighter, lang, theme, code, options, onStart, onEnd, ...props }: ShikiMagicMoveProps = $props()
+  const machine = createMagicMoveMachine(
+    code => codeToKeyedTokens(highlighter, code, { lang, theme }, options?.lineNumbers),
+    options,
+  )
+  const result = $derived(machine.commit(code))
 </script>
 
 <ShikiMagicMoveRenderer
-	tokens={result.current}
-	previous={result.previous}
-	{options}
-	{onStart}
-	{onEnd}
-	{...props}
+  tokens={result.current}
+  previous={result.previous}
+  {options}
+  {onStart}
+  {onEnd}
+  {...props}
 />
