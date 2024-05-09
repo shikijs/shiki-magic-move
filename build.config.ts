13dcd3411743aae23d77a241357b8b42f20e39ac
paoloricciuti <ricciutipaolo@gmail.com>
2024-05-09 13:39:57 +0200

    fix: svelte types generation during build

--- build.config.ts
+++ build.config.ts
@@ -33,6 +33,7 @@ export default defineBuildConfig({
       await fs.writeFile('dist/svelte.mjs', 'export * from "./svelte/index.mjs"\n', 'utf-8')
       await fs.writeFile('dist/svelte.d.ts', 'export * from "./svelte/index.mjs"\n', 'utf-8')
       await fs.writeFile('dist/svelte.d.mts', 'export * from "./svelte/index.mjs"\n', 'utf-8')
+      await fs.copyFile('dist/svelte/index.d.ts', 'dist/svelte/index.d.mts')
     },
   },
 })
