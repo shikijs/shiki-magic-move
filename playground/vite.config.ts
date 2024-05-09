92714a8c44724df9f4848227c0304d5667e91ec3
Anthony Fu <anthonyfu117@hotmail.com>
2024-05-09 17:37:23 +0200

    chore: update

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,12 +1,19 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte as Svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'shiki-magic-move/core': fileURLToPath(new URL('../src/core.ts', import.meta.url)),
+      'shiki-magic-move/renderer': fileURLToPath(new URL('../src/renderer.ts', import.meta.url)),
+    },
+  },
   plugins: [
     Vue(),
     UnoCSS(),
-    svelte(),
+    Svelte(),
   ],
 })
