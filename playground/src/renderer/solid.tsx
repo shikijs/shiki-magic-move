ba4330065efccdac29e0aa10fbebacd779975811
Yaroslav Lapin <jlarky@gmail.com>
2024-10-19 14:16:01 -0600

    simplify solid playground
    This should not be introducing any changes, just a cleanup after looking at other implementations :)

--- playground/src/renderer/solid.tsx
+++ playground/src/renderer/solid.tsx
@@ -1,67 +1,36 @@
 /** @jsxImportSource solid-js */
 
+import type { ShikiMagicMoveProps } from '../../../src/solid/ShikiMagicMove'
 import type { RendererFactory, RendererFactoryResult } from './types'
-import { createEffect, createSignal } from 'solid-js'
+import { createSignal } from 'solid-js'
 import { render } from 'solid-js/web'
-import { shallowReactive, watch } from 'vue'
 import { ShikiMagicMove } from '../../../src/solid'
 
 export const createRendererSolid: RendererFactory = (options): RendererFactoryResult => {
-  const props = shallowReactive<any>({
+  const [props, setProps] = createSignal({
     onStart: options.onStart,
     onEnd: options.onEnd,
-  })
+  } as ShikiMagicMoveProps)
 
-  function App() {
-    const [code, setCode] = createSignal(props.code as string)
-    const [highlighter, setHighlighter] = createSignal(props.highlighter)
-    const [lang, setLang] = createSignal(props.lang)
-    const [theme, setTheme] = createSignal(props.theme)
-    const [className, setClassName] = createSignal(props.class)
-    const [options, setOptions] = createSignal(props.options)
-
-    createEffect(() => {
-      watch(props, () => {
-        // Force Solid to re-render
-        setCode(props.code)
-        setHighlighter(props.highlighter)
-        setLang(props.lang)
-        setTheme(props.theme)
-        setClassName(props.class)
-        setOptions(props.options)
-      })
-    })
-
-    return (
-      <>
-        <ShikiMagicMove
-          highlighter={highlighter()}
-          code={code()}
-          class={className()}
-          lang={lang()}
-          theme={theme()}
-          options={options()}
-        />
-      </>
-    )
-  }
-
-  let dispose = () => {}
+  let dispose: (() => void) | undefined
 
   return {
     mount: (element, payload) => {
-      Object.assign(props, payload)
+      setProps(props => ({ ...props, ...payload }))
       dispose = render(
-        () => <App />,
+        () => <ShikiMagicMove {...props()} />,
         element,
       )
     },
 
     update: (payload) => {
-      Object.assign(props, payload)
+      setProps(props => ({ ...props, ...payload }))
     },
     dispose: () => {
-      dispose?.()
+      if (dispose) {
+        dispose?.()
+        dispose = undefined
+      }
     },
   }
 }
